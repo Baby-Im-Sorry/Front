4b7a1a0343dcb50afafc99b87b45b2eb43643fb6
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-23 00:40:40 +0900

    add _new method for test

--- lib/briefing.dart
+++ lib/briefing.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:bis_front/home.dart';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:http/http.dart' as http;
@@ -18,6 +19,7 @@ class _ChatScreenState extends State<ChatScreen> {
   WebSocketChannel? _channel;
   // final channel = WebSocketChannel.connect(Uri.parse(widget.websocketUrl));
   List<String> messages = [];
+  String request_id = "";
 
   Future<void> _getCurrentBriefing() async {
     String url = 'http://127.0.0.1:8000/getCurrentBriefing?username=${widget.username}';
@@ -26,6 +28,7 @@ class _ChatScreenState extends State<ChatScreen> {
       // print(response.body);
       var data = jsonDecode(response.body);
       // print(data);
+      request_id = data['request_id'];
       var content = data['content'] as List<dynamic>;
       var stringList = content.map((e) => e as String).toList();
       // print(content);
@@ -40,20 +43,47 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  void _connectToWebSocket() {
-    _getCurrentBriefing().then((value) {
-      // 웹소켓 채널을 변수에 저장
-      _channel = WebSocketChannel.connect(Uri.parse(widget.websocketUrl));
-      // 필요한 추가 로직 (예: 상태 업데이트, 메시지 수신 처리 등)
-    });
+  Future<void> _endBriefing(context) async {
+    String url = 'http://127.0.0.1:8000/endBriefing';
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content_Type': 'application/x-www-form-urlencoded',
+      },
+      body: {
+        'username': widget.username,
+        'request_id': request_id,
+      },
+    );
+    if (response.statusCode == 200) {
+      _channel?.sink.close();
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+            builder: (context) => HomeScreen(username: widget.username)),
+            (Route<dynamic> route) => false,
+      );
+    }
+  }
 
+  // void _connectToWebSocket() {
+  //   _getCurrentBriefing().then((value) {
+  //     // 웹소켓 채널을 변수에 저장
+  //     _channel = WebSocketChannel.connect(Uri.parse(widget.websocketUrl));
+  //     // 필요한 추가 로직 (예: 상태 업데이트, 메시지 수신 처리 등)
+  //   });
+  // }
+
+  void _new() {
+    _channel = WebSocketChannel.connect(Uri.parse(widget.websocketUrl));
   }
 
   @override
   void initState() {
     super.initState();
     // setState(() {
-    _connectToWebSocket();
+    // _connectToWebSocket();
+    _new();
     // });
 
   }
@@ -63,6 +93,14 @@ class _ChatScreenState extends State<ChatScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Briefing'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              print('pressed');
+            },
+            child: const Text('Stop'),
+          ),
+        ],
       ),
       body: StreamBuilder(
         stream: _channel?.stream,
