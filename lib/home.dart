9e64d7ade1ae097975ad8ac090779b52407d7489
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-27 05:02:59 +0900

    add websocket url for activebf.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:bis_front/activebriefing.dart';
 import 'package:bis_front/briefing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -72,23 +75,9 @@ class _HomeScreenState extends State<HomeScreen> {
   // }
 
   void _startBriefing(context) {
-    // String url = 'http://13.125.63.186:8000/startBriefing';
-    // String url = 'http://127.0.0.1:8000/startBriefing';
-    // final response = await http.post(
-    //   Uri.parse(url),
-    //   headers: {
-    //     'Content-Type': 'application/x-www-form-urlencoded',
-    //   },
-    //   body: {
-    //     'username' : widget.username,
-    //     'interval' : _controller.text,
-    //     'endtime' : selectedTime.format(context),
-    //   },
-    // );
     final interval = _controller.text;
     final endtime = selectedTime.format(context);
-    final websocketUrl = 'ws://127.0.0.1:8000/ws?username=${widget.username}&interval=$interval&endtime=$endtime';
-
+    final websocketUrl = 'ws://10.0.2.2:8000/ws?username=${widget.username}&interval=$interval&endtime=$endtime';
     Navigator.push(
         context,
         MaterialPageRoute(
@@ -98,10 +87,23 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    String activeWebsocketUrl = 'ws://10.0.2.2:8000/reloadBriefing?username=${widget.username}';
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Home'),
+        actions: [
+          TextButton(
+            child: const Text('Briefing'),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ActiveChatScreen(websocketUrl: activeWebsocketUrl, username: widget.username))
+              );
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Container(
